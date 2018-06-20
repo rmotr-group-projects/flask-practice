24d660e21d99b8a077cfad8c93a206e95dc75ab4
Peter Ryan <peter.e.ryan@gmail.com>
2018-06-20 23:55:49 +0000

    flask practice complete

--- run_app.py
+++ run_app.py
@@ -12,11 +12,13 @@ def welcome_view():
 @app.route('/sum/<int:first_number>/<int:second_number>')
 def sum_of_two_numbers(first_number, second_number):
     """Implement a view that receives two numbers and returns the sum of them"""
+    
     total = first_number + second_number
     return 'The sum of {} and {} is: {}'.format(first_number, second_number, total)
 
 
-def build_username():
+@app.route('/username/<string:first_name>/<string:last_name>')
+def build_username(first_name, last_name):
     """
         Implement a view that receives user's first name and last name,
         and returns its username built with first letter of the first name,
@@ -24,8 +26,8 @@ def build_username():
 
         i.e: username for "Elon Musk" would be "emusk"
     """
-    pass
-
+    return '{}{}'.format(first_name.lower()[0], last_name.lower())
+    
 @app.route('/user')
 def search_user():
     """
@@ -37,8 +39,11 @@ def search_user():
     # HINT: to access the query params you'll need to use request.args.get()
     # function imported from flask
     users = ['Jack', 'Morgan', 'Moe', 'Steve']
-    pass
-
+    
+    search_term = request.args.get('search')
+    
+    user_count = [user for user in users if search_term in user.lower()]
+    return 'Found {} users that match with search: "{}"'.format(len(user_count), search_term) 
 
 if __name__ == '__main__':
     app.debug = True
