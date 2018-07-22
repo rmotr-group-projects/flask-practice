37f8d024afbc23c0332124b7b70d5b4b8d9962fa
Craig Rupp <craigrupp@utexas.edu>
2018-07-22 19:35:11 +0000

    Tests coded for but not appropriately tested as requirements file isn't installing

--- run_app.py
+++ run_app.py
@@ -15,8 +15,8 @@ def sum_of_two_numbers(first_number, second_number):
     total = first_number + second_number
     return 'The sum of {} and {} is: {}'.format(first_number, second_number, total)
 
-
-def build_username():
+@app.route('/username/<str:first_name>/<str:last_name>')
+def build_username(first_name, last_name):
     """
         Implement a view that receives user's first name and last name,
         and returns its username built with first letter of the first name,
@@ -24,7 +24,7 @@ def build_username():
 
         i.e: username for "Elon Musk" would be "emusk"
     """
-    pass
+    return first_name[0].lower() + last_name.lower()
 
 @app.route('/user')
 def search_user():
@@ -37,7 +37,9 @@ def search_user():
     # HINT: to access the query params you'll need to use request.args.get()
     # function imported from flask
     users = ['Jack', 'Morgan', 'Moe', 'Steve']
-    pass
+    search_param = request.args.get('search')
+    search_match = [user for user in users if search_param.lower() in user.lower()]
+    return 'Found {} users that match with search: "{}"'.format(str(len(search_match)), search_param)
 
 
 if __name__ == '__main__':
