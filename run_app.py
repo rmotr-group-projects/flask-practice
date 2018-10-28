464793f7153f09b63e9abf82082e5894bda0561c
kufopo <kufopo@gmail.com>
2018-10-28 04:38:35 +0000

    requirement dont need ipd

--- run_app.py
+++ run_app.py
@@ -15,8 +15,8 @@ def sum_of_two_numbers(first_number, second_number):
     total = first_number + second_number
     return 'The sum of {} and {} is: {}'.format(first_number, second_number, total)
 
-
-def build_username():
+@app.route('/username/<fname>/<sname>')
+def build_username(fname,sname):
     """
         Implement a view that receives user's first name and last name,
         and returns its username built with first letter of the first name,
@@ -24,7 +24,8 @@ def build_username():
 
         i.e: username for "Elon Musk" would be "emusk"
     """
-    pass
+    result = str(fname[0]).lower()+str(sname).lower()
+    return result
 
 @app.route('/user')
 def search_user():
@@ -37,7 +38,12 @@ def search_user():
     # HINT: to access the query params you'll need to use request.args.get()
     # function imported from flask
     users = ['Jack', 'Morgan', 'Moe', 'Steve']
-    pass
+    tt  = request.args.get('search')
+    count = 0
+    for name in users:
+        if tt.lower() in name.lower():
+            count +=1
+    return 'Found {} users that match with search: "{}"'.format(count, tt)
 
 
 if __name__ == '__main__':
