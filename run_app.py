ccf01a492813ddb83424c62f89da6b93a0c857ea
gmrkss <parlamasgeorge@gmail.com>
2019-06-03 20:57:50 +0000

    Passing 3/4 tests

--- run_app.py
+++ run_app.py
@@ -16,28 +16,31 @@ def sum_of_two_numbers(first_number, second_number):
     return 'The sum of {} and {} is: {}'.format(first_number, second_number, total)
 
 
-def build_username():
-    """
-        Implement a view that receives user's first name and last name,
-        and returns its username built with first letter of the first name,
-        concatenated with the last name.
+@app.route('/username/<first_name>/<last_name>')
+def build_username(first_name, last_name):
+    first = first_name.lower()[0]
+    second = last_name.lower()
+    return str(first+second)
 
-        i.e: username for "Elon Musk" would be "emusk"
-    """
-    pass
 
-@app.route('/user')
-def search_user():
-    """
-        Implement a view that receives a '?search=' query parameter in the URL,
-        and returns the amount of users in the given users_list that contains
-        that search string in their names.
-        i.e: /user?search=mo will return 'Found 2 users that match with search "mo"'
-    """
-    # HINT: to access the query params you'll need to use request.args.get()
-    # function imported from flask
+@app.route('/user?search=<param>')
+def search_user(param):
+
+#     """
+#         Implement a view that receives a '?search=' query parameter in the URL,
+#         and returns the amount of users in the given users_list that contains
+#         that search string in their names.
+#         i.e: /user?search=mo will return 'Found 2 users that match with search "mo"'
+#     """
+#     # HINT: to access the query params you'll need to use request.args.get()
+#     # function imported from flask
+    request.args.get()
     users = ['Jack', 'Morgan', 'Moe', 'Steve']
-    pass
+    number = 0
+    for user in users:
+        if param in user:
+            number+=1
+    return 'Found {} users that match with search "{}"'.format(number, param)
 
 
 if __name__ == '__main__':
